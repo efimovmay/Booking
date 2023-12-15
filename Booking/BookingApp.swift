c12a93d4b5c3c8f7d495c5bee43b9dc6d95e1e96
Aleksey Efimov <smaylik121@gmail.com>
2023-12-15 11:55:58 +0300

    Initial Commit

---
+++ Booking/BookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookingApp.swift
+//  Booking
+//
+//  Created by Aleksey Efimov on 15.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
